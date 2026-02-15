a1e64f9aaba41cd83dded31a85da3ab3a0bd6537
Rabbi111-cloud <diggingdeep0002@gmail.com>
2026-02-15 09:05:46 +0100

    Create Home component for main page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>PulseGuard AI</h1>
+      <p>Global Healthcare Intelligence Platform</p>
+    </main>
+  );
+}
